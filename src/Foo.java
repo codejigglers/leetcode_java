0b2bd860fbbc4e3363003d4c391807219c856b37
Vardan Gupta <vardan.gupta@as-MacBook-Pro.local>
2018-11-17 19:07:00 -0800

    17November_Checkin

---
+++ src/Foo.java
@@ -0,0 +1,10 @@
+public class Foo {
+
+
+    private static Foo foo;
+
+    private Foo() {
+    }
+
+}
+
---
+++ src/Test.java
@@ -0,0 +1,25 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1, 5, 3, 7, 3, 6, 2};
+        int x = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            if ( arr[i] >= 6 ) {
+                i++;
+            } else {
+                x += arr[i];
+            }
+        }
+
+        System.out.println(result(4));
+    }
+
+    static int result(int n) {
+        if (n == 1) {
+            return 2;
+        } else {
+            return 2 * result(n-1);
+        }
+    }
+}
